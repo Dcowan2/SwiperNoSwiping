d6938c6f7a9500ea67c86345012052e41df50dc0
Dcowan2 <danny.cowan99@gmail.com>
2022-05-10 11:13:21 -0400

    commit

---
+++ puppyswipe-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PuppyCards from './PuppyCards';
+
+
+function App() {
+  return (
+  <div className="app">
+    <PuppyCards />
+  </div>    
+  );
+}
+
+export default App;
---
+++ puppyswipe-frontend/src/PuppyCards.js
@@ -0,0 +1,46 @@
+import React, { useState,useEffect } from "react";
+import TinderCard from "react-tinder-card";
+import "./PuppyCards.css";
+import axios from "./axios";
+
+function PuppyCards() {
+  const [dog, setDog] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+      setDog(req.data);
+    }
+    fetchData();
+  }, []);
+
+  const swiped = (direction, nameToDelete) => {
+  };
+  const outOfFrame = (name) => {
+  };
+
+  return (
+    <div className="puppyCards">
+      <div className="puppyCards">
+        {dog.map((puppy) => (
+          <TinderCard
+            className="swipe"
+            key={puppy.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, puppy.name)}
+            onCardLeftScreen={() => outOfFrame(puppy.name)}
+          >
+            <div
+              className="card"
+              style={{ backgroundImage: `url(${puppy.imgUrl})` }}
+            >
+              <h3>{puppy.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default PuppyCards;
