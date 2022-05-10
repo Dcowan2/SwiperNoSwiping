d9dfde33391bd9370808640cbbe8229b0bbd741c
Dcowan2 <77221491+Dcowan2@users.noreply.github.com>
2022-05-10 10:53:29 -0400

    Add files via upload

---
+++ puppyswipe-backend/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
+import dotenv from "dotenv";
+
+// app config
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://admin:passwordpassword@cluster0.eehha.mongodb.net/owners?retryWrites=true&w=majority'
+
+
+// middlewares
+app.use(express.json());
+app.use(Cors());
+
+// DB config
+mongoose.connect(connection_url, {
+});
+
+// API endpoints
+app.get("/", (req, res) => {
+  res.status(200).send("working");
+});
+
+app.post("/tinder/cards", (req, res) => {
+  const Card = req.body;
+
+  Cards.create(Card, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+//Listener
+app.listen(port, () => {
+  console.log(`listening on localhost ${port}`);
+});
