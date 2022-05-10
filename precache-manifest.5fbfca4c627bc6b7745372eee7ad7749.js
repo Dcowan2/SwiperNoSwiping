83b06a17d9c6f908a5d5899adf04d84354da27b3
mikelong22 <95258756+mikelong22@users.noreply.github.com>
2022-05-10 12:04:49 -0400

    Updates

---
+++ precache-manifest.5fbfca4c627bc6b7745372eee7ad7749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42ff4d793a0a84992be266b94da92d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2757d70021cda1436c8e",
+    "url": "/static/css/main.31a76691.chunk.css"
+  },
+  {
+    "revision": "8627206ba162f966982d",
+    "url": "/static/js/2.ff445c41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff445c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2757d70021cda1436c8e",
+    "url": "/static/js/main.ec8d3c65.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
